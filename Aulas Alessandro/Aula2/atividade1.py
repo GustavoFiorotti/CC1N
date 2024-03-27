e2bc2a934a3254f1873a8846d866cc55ee0202a9
Gustavo Fiorotti <125517184+GustavoFiorotti@users.noreply.github.com>
2024-03-27 01:39:35 +0000

    alessandro

---
+++ Aulas Alessandro/Aula2/atividade1.py
@@ -0,0 +1,8 @@
+print (f"Calcular e exibir a hipotenusa (A) de um triângulo retângulo de catetos B e C.")
+
+B = float(input("Insira o valor de B: "))
+
+
+C = float(input("Insira o valor de C: ")
+          
+print (f"O valor da Hipotenusa é {(B ** 2) + (C ** 2)  }.")
